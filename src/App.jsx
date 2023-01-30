236e503442d12abff1e0042854540242ebb85fb2
Pilar Rosas <rosas190688@gmail.com>
2023-01-30 10:28:13 -0300

    Primer Entrega del Proyecto Final

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <nav className="App">
+    <label>Cross Commerce</label>
+     <p>Gorras</p>
+     <p>Remeras</p>
+    </nav>
+    <section></section>
+    </>
+  )
+}
+
+export default App
